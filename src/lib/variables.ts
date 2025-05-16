46b522357b68915268b8f3147343ca4fb72cf8a1
AsgardPavlov <pasgard9@gmail.com>
2025-05-17 00:09:16 +0500

    add variables

---
+++ src/lib/variables.ts
@@ -0,0 +1,5 @@
+export const LINKEDIN_URL = "https://www.linkedin.com/company/neuroguard-ai";
+export const INSTAGRAM_URL = "https://www.linkedin.com/company/neuroguard-ai";
+
+export const SUNCAR_SITE_URL = "https://densaulyk.kz"
+export const SHASHKIN_SITE_URL = "https://shashkinclinic.org"
