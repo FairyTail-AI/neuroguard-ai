3f489bdc860c2f5be46235e029cc26ecbf515f02
AsgardPavlov <pasgard9@gmail.com>
2025-05-19 11:21:33 +0500

    comment out unnecessary code

--- next.config.mjs
+++ next.config.mjs
@@ -3,40 +3,40 @@ const nextConfig = {
     output: 'standalone',
     reactStrictMode: true,
 
-    async headers() {
-        return [
-            {
-                source: '/.well-known/acme-challenge/:path*',
-                headers: [
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: '*',
-                    },
-                ],
-            },
-            {
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'Strict-Transport-Security',
-                        value: 'max-age=63072000; includeSubDomains; preload'
-                    },
-                    {
-                        key: 'X-Content-Type-Options',
-                        value: 'nosniff'
-                    },
-                    {
-                        key: 'X-Frame-Options',
-                        value: 'SAMEORIGIN'
-                    },
-                    {
-                        key: 'X-XSS-Protection',
-                        value: '1; mode=block'
-                    },
-                ],
-            },
-        ]
-    },
+    // async headers() {
+    //     return [
+    //         {
+    //             source: '/.well-known/acme-challenge/:path*',
+    //             headers: [
+    //                 {
+    //                     key: 'Access-Control-Allow-Origin',
+    //                     value: '*',
+    //                 },
+    //             ],
+    //         },
+    //         {
+    //             source: '/:path*',
+    //             headers: [
+    //                 {
+    //                     key: 'Strict-Transport-Security',
+    //                     value: 'max-age=63072000; includeSubDomains; preload'
+    //                 },
+    //                 {
+    //                     key: 'X-Content-Type-Options',
+    //                     value: 'nosniff'
+    //                 },
+    //                 {
+    //                     key: 'X-Frame-Options',
+    //                     value: 'SAMEORIGIN'
+    //                 },
+    //                 {
+    //                     key: 'X-XSS-Protection',
+    //                     value: '1; mode=block'
+    //                 },
+    //             ],
+    //         },
+    //     ]
+    // },
 }
 
 export default nextConfig
